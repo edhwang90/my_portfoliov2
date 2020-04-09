3071185641af8f65a9c696a5239363263da7f72c
EDH <ambp@Apples-MacBook-Pro.local>
2020-04-09 18:27:52 -0400

    showcase section
    showcase sections mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['Portfolio', 'Edmond Hwang', 'Edy Hwang']} />
       <section className="landing wrapper-container">
-        <h3 className="hero-text">
-          Hello World! My name is <strong>Edy Hwang</strong>, and I am an experienced <strong>Front&#x2011;End Developer</strong> with a passion for <strong>User Experience</strong>.
-        </h3>
+        <h1 className="hero-text">
+        Hello World! My name is <strong>Edy Hwang</strong>, and I am an experienced <strong>Front&#x2011;End Developer</strong> with a passion for <strong>User Experience</strong>.
+        </h1>
         <div className="hero-img">
           <img className="hero desktop-only" src="https://res.cloudinary.com/dkcdueneq/image/upload/v1577126229/waving-hand_qdyn4e.png"/>
         </div>
