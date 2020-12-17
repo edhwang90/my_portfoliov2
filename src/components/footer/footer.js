6763c38a3e4862cfe1a0fe600a2355aa85bb38e8
EDH <ambp@Apples-MacBook-Pro.local>
2020-12-17 12:45:47 +0900

    build
    build

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,30 +6,30 @@ import './footer.scss';
 const Footer = () => (
   <div id="Footer" className="footer-container">
     <ul className="navigation">
-    <li>
-      <Link className="link" to="/" title="Home">Home</Link>
-    </li>
-    <li>
-      <a
-        className="link"
-        title="Resume"
-        href="/edh_resume.pdf"
-        rel="noopener noreferrer"
-        target="_blank">
-        Resume
-      </a>
-    </li>
-    <li>
-      <a
-        className="link"
-        title="GitHub"
-        href="https://github.com/edhwang90"
-        rel="noopener noreferrer"
-        target="_blank">
-        GitHub
-      </a>
-    </li>
-  </ul>
+      <li>
+        <Link className="link" to="/" title="Home">Home</Link>
+      </li>
+      <li>
+        <a
+          className="link"
+          title="Resume"
+          href="/edh_resume.pdf"
+          rel="noopener noreferrer"
+          target="_blank">
+          Resume
+        </a>
+      </li>
+      <li>
+        <a
+          className="link"
+          title="GitHub"
+          href="https://github.com/edhwang90"
+          rel="noopener noreferrer"
+          target="_blank">
+          GitHub
+        </a>
+      </li>
+    </ul>
 
     <a
       className="link rounded inverted"
